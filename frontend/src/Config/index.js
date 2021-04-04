38f4dd925b00ab02abc3ac04c48e9a9d81abf65a
İlker Demir <ilker.demir@smartmessage.com>
2021-04-04 22:40:59 +0300

    frontend/base styles add

--- frontend/src/Config/index.js
+++ frontend/src/Config/index.js
@@ -2,4 +2,5 @@ export default {
   pathname: process.env.REACT_APP_PATHNAME,
   pageTitle: process.env.REACT_APP_PAGE_TITLE,
   client: process.env.REACT_APP_CLIENT,
+  publicUrl: process.env.PUBLIC_URL,
 };
--- frontend/src/Shared/Layouts/NotAuth.js
+++ frontend/src/Shared/Layouts/NotAuth.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import Config from 'Config';
 
 export const NotAuthLayout = ({ component: Component, ...rest }) => {
   return (
-    <div>
-      <Component {...rest} />
-    </div>
+    <Row className="not_auth_layout">
+      <Col span={14} className="not_auth_layout--left">
+        <Component {...rest} />
+      </Col>
+      <Col span={10}>
+        <img
+          className="not_auth_layout--right"
+          src={`${Config.publicUrl}/images/NotAuthLayout/not-auth-one.jpg`}
+          height="400"
+          width="400"
+          alt="not-auth"
+        />
+      </Col>
+    </Row>
   );
 };
