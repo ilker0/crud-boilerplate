f7a8768d0c7b1c3a28625fa7c389ea44f5dacb7b
İlker Demir <ilker.demir@smartmessage.com>
2021-05-02 02:12:56 +0300

    frontednd/tables added

--- frontend/src/Roles/Pages/List.js
+++ frontend/src/Roles/Pages/List.js
@@ -1,5 +1,88 @@
 import React from 'react';
+import { Row, Col, Select, Table, Dropdown, Menu, Button } from 'antd';
+import { FileOutlined, PlusOutlined } from '@ant-design/icons';
 
 export function RoleList() {
-  return <h1>Roles</h1>;
+  const data = [
+    {
+      key: '1',
+      firstName: 'John',
+      lastName: 'Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      firstName: 'Jim',
+      lastName: 'Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      firstName: 'Joe',
+      lastName: 'Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+
+  const exportMenu = (
+    <Menu>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          Excel file
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com"
+        >
+          Csv file
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
+  return (
+    <Row>
+      <Col span={24} className="b-white u-p-4 shadow-primary u-m-l-a">
+        <div className="flex u-m-b-4 justify-between">
+          <div>
+            <Dropdown overlay={exportMenu} placement="bottomLeft" arrow>
+              <Button>
+                <FileOutlined />
+                Export
+              </Button>
+            </Dropdown>
+            <Button className="u-m-l-3" type="primary">
+              <PlusOutlined />
+              New role
+            </Button>
+          </div>
+          <Select style={{ width: 110 }} defaultValue="10">
+            <Select.Option value="10">10 / Page</Select.Option>
+            <Select.Option value="20">20 / Page</Select.Option>
+            <Select.Option value="30">30 / Page</Select.Option>
+            <Select.Option value="40">40 / Page</Select.Option>
+            <Select.Option value="50">50 / Page</Select.Option>
+          </Select>
+        </div>
+        <Table dataSource={data} rowSelection>
+          <Table.Column title="Age" dataIndex="age" key="age" />
+          <Table.Column title="Address" dataIndex="address" key="address" />
+          <Table.Column title="Tags" dataIndex="tags" key="tags" />
+          <Table.Column title="Action" key="action" />
+        </Table>
+      </Col>
+    </Row>
+  );
 }
--- frontend/src/Users/Pages/List.js
+++ frontend/src/Users/Pages/List.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Row, Col, Button, Tooltip } from 'antd';
+import { Row, Col, Button, Tooltip, Select, Menu, Dropdown, Table } from 'antd';
 import { UsersFilter } from 'Users/Components';
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PlusOutlined,
+  FileOutlined,
+} from '@ant-design/icons';
 
 export function UserList() {
   const { t } = useTranslation();
@@ -12,6 +17,56 @@ export function UserList() {
   const handleFilterVisible = () => {
     setisVisibleFilters(!isVisibleFilters);
   };
+
+  const data = [
+    {
+      key: '1',
+      firstName: 'John',
+      lastName: 'Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      firstName: 'Jim',
+      lastName: 'Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      firstName: 'Joe',
+      lastName: 'Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+
+  const exportMenu = (
+    <Menu>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          Excel file
+        </a>
+      </Menu.Item>
+      <Menu.Item>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com"
+        >
+          Csv file
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
 
   return (
     <Row>
@@ -31,19 +86,53 @@ export function UserList() {
             xs={24}
             sm={24}
             md={24}
-            lg={isVisibleFilters ? 14 : 24}
-            xl={isVisibleFilters ? 19 : 24}
+            lg={isVisibleFilters ? 13 : 20}
+            xl={isVisibleFilters ? 18 : 24}
+            className="b-white u-p-4 shadow-primary u-m-l-a"
           >
-            <Tooltip title={isVisibleFilters ? 'Gizle' : 'Göster'}>
-              <Button onClick={handleFilterVisible}>
-                Filtreler{' '}
-                {isVisibleFilters ? (
-                  <MenuFoldOutlined />
-                ) : (
-                  <MenuUnfoldOutlined />
-                )}
-              </Button>
-            </Tooltip>
+            <div className="flex u-m-b-4 justify-between">
+              <div>
+                <Tooltip title={isVisibleFilters ? 'Gizle' : 'Göster'}>
+                  <Button onClick={handleFilterVisible}>
+                    Filtreler
+                    {isVisibleFilters ? (
+                      <MenuFoldOutlined />
+                    ) : (
+                      <MenuUnfoldOutlined />
+                    )}
+                  </Button>
+                </Tooltip>
+                <Dropdown
+                  className="u-m-l-3"
+                  overlay={exportMenu}
+                  placement="bottomLeft"
+                  arrow
+                >
+                  <Button>
+                    <FileOutlined />
+                    Export
+                  </Button>
+                </Dropdown>
+                <Button className="u-m-l-3" type="primary">
+                  <PlusOutlined />
+                  New user
+                </Button>
+              </div>
+
+              <Select style={{ width: 110 }} defaultValue="10">
+                <Select.Option value="10">10 / Page</Select.Option>
+                <Select.Option value="20">20 / Page</Select.Option>
+                <Select.Option value="30">30 / Page</Select.Option>
+                <Select.Option value="40">40 / Page</Select.Option>
+                <Select.Option value="50">50 / Page</Select.Option>
+              </Select>
+            </div>
+            <Table dataSource={data} rowSelection>
+              <Table.Column title="Age" dataIndex="age" key="age" />
+              <Table.Column title="Address" dataIndex="address" key="address" />
+              <Table.Column title="Tags" dataIndex="tags" key="tags" />
+              <Table.Column title="Action" key="action" />
+            </Table>
           </Col>
         </Row>
       </Col>
